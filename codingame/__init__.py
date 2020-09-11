6f9612a4e8101334e8284721a3b778ec6b7414f4
Takos22 <tora.akos@gmail.com>
2020-09-11 18:45:34 +0200

    Update version

--- codingame/__init__.py
+++ codingame/__init__.py
@@ -7,12 +7,18 @@ Basic wrapper for the undocumented CodinGame API.
 
 __title__ = 'codingame'
 __author__ = 'takos22'
-__version__ = '0.0.1'
+__version__ = '0.0.1a'
 
 __all__ = [
     "Client",
     "CodinGamer",
 ]
 
+from collections import namedtuple
+
 from .client import Client
 from .codingamer import CodinGamer
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=0, micro=1, releaselevel='alpha', serial=0)
