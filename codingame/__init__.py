169efee9afcebfb2c6e30756f99f194fb51d69b3
Takos22 <tora.akos@gmail.com>
2020-09-12 19:27:28 +0200

    Add exceptions import in __init__.py

--- codingame/__init__.py
+++ codingame/__init__.py
@@ -5,20 +5,23 @@ CodinGame API Wrapper
 Basic wrapper for the undocumented CodinGame API.
 """
 
-__title__ = 'codingame'
-__author__ = 'takos22'
-__version__ = '0.1.0'
+__title__ = "codingame"
+__author__ = "takos22"
+__version__ = "0.1.0"
 
 __all__ = [
     "Client",
     "CodinGamer",
+    "CodinGameAPIError",
+    "CodinGamerNotFound",
 ]
 
 from collections import namedtuple
 
 from .client import Client
 from .codingamer import CodinGamer
+from .exceptions import CodinGameAPIError, CodinGamerNotFound
 
-VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='', serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="", serial=0)
