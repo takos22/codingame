02609fc4fb0e9bb82daf1e82b3705e0d961b1b7d
Takos22 <tora.akos@gmail.com>
2020-09-12 19:42:05 +0200

    Update __init__.py

--- codingame/__init__.py
+++ codingame/__init__.py
@@ -5,6 +5,12 @@ CodinGame API Wrapper
 Basic wrapper for the undocumented CodinGame API.
 """
 
+from collections import namedtuple
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="", serial=0)
+
 __title__ = "codingame"
 __author__ = "takos22"
 __version__ = "0.1.0"
@@ -16,12 +22,6 @@ __all__ = [
     "CodinGamerNotFound",
 ]
 
-from collections import namedtuple
-
 from .client import Client
 from .codingamer import CodinGamer
 from .exceptions import CodinGameAPIError, CodinGamerNotFound
-
-VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="", serial=0)
