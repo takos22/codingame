27098711cad10453ff156855a77ce67dff97e162
Takos22 <tora.akos@gmail.com>
2020-09-12 19:49:22 +0200

    Fix docs Why is it so hard to make docs work

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ release = version
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
+    'sphinx.ext.intersphinx',
     "exception_hierarchy",
 ]
 
@@ -64,7 +65,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build"]
 
 
 # -- Options for HTML output -------------------------------------------------
