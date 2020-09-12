e41e5edf91a272349112d578fc4d9cc4216dd3fd
Takos22 <tora.akos@gmail.com>
2020-09-12 20:43:07 +0200

    Update docstring

--- codingame/client.py
+++ codingame/client.py
@@ -13,10 +13,26 @@ class Client:
     def __init__(self):
         self._session = requests.Session()
 
-    def codingamer(self, codingamer_handle: str) -> Optional[CodinGamer]:
-        """Get a CodinGamer from his public handle."""
+    def codingamer(self, codingamer_handle: str) -> CodinGamer:
+        """Get a CodinGamer from his public handle.
+
+        Parameters
+        -----------
+        codingamer_handle: :class:`str`
+            The CodinGamer's public handle (39 character long hexadecimal string).
+
+        Raises
+        ------
+        :exc:`.CodinGamerNotFound`
+            The CodinGamer with the given public handle isn't found.
+
+        Returns
+        --------
+        :class:`CodinGamer`
+            The CodinGamer.
+        """
 
         r = self._session.post(Endpoints.CodinGamer, json=[codingamer_handle])
         if r.json() is None:
-            raise CodinGamerNotFound(f"No CodinGamer with handle {repr(codingamer_handle)}")
+            raise CodinGamerNotFound(f"No CodinGamer with handle {codingamer_handle!r}")
         return CodinGamer(**r.json()["codingamer"])
