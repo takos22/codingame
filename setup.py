211c554478ddd121d7b7366ddec6e02967c22970
Takos <59511469+takos22@users.noreply.github.com>
2020-09-13 14:29:21 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setup(
     install_requires=requirements,
     python_requires=">=3.5",
     classifiers=[
-        "Programming Language :: Python"
-        "Programming Language :: Python :: 3.5"
-        "Programming Language :: Python :: 3.6"
-        "Programming Language :: Python :: 3.7"
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
