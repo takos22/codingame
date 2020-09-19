07006e6c39da16898b05af0b978323e1790babd2
Takos22 <tora.akos@gmail.com>
2020-09-19 14:07:15 +0200

    v0.3.0

--- codingame/__init__.py
+++ codingame/__init__.py
@@ -9,11 +9,11 @@ from collections import namedtuple
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel="", serial=0)
+version_info = VersionInfo(major=0, minor=3, micro=0, releaselevel="", serial=0)
 
 __title__ = "codingame"
 __author__ = "takos22"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 __all__ = [
     "Client",
