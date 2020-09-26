fe317e6f1ac3006a328e96b043e72e85bed8970c
Takos22 <tora.akos@gmail.com>
2020-09-26 14:31:33 +0200

    Update examples

--- examples/example_clash_of_code.py
+++ examples/example_clash_of_code.py
@@ -2,8 +2,11 @@ import codingame
 
 client = codingame.Client()
 
-# get a clash of code from its public handle
+# get a pending public clash of code
+coc = client.get_pending_clash_of_code()
+# or get a clash of code from its public handle
 coc = client.get_clash_of_code("clash of code handle here")
+
 print(coc)
 print(coc.join_url)
 print(coc.modes)
