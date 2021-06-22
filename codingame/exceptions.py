08856dbe0ccf18993cb46b67a7d0ec8a54f13f3c
Takos22 <takos2210@gmail.com>
2021-06-22 22:36:57 +0200

    Add login errors

--- codingame/exceptions.py
+++ codingame/exceptions.py
@@ -3,6 +3,43 @@ class CodinGameAPIError(Exception):
 
     def __init__(self, message: str):
         self.message = message
+        super().__init__(message)
+
+
+class LoginError(CodinGameAPIError):
+    """Raised when the login data is incorrect."""
+
+    @classmethod
+    def from_id(cls, id: int, message: str):
+        errors = {
+            332: EmailRequired,
+            334: MalformedEmail,
+            336: PasswordRequired,
+            393: EmailNotLinked,
+            396: IncorrectPassword,
+        }
+        return errors.get(id, cls)(message)
+
+
+class EmailRequired(LoginError):
+    """Raised when the email given at login is empty."""
+
+
+class MalformedEmail(LoginError):
+    """Raised when the email given at login isn't well formed."""
+
+
+class PasswordRequired(LoginError):
+    """Raised when the password given at login is empty."""
+
+
+class EmailNotLinked(LoginError):
+    """Raised when the email given at login isn't linked to a CodinGamer
+    account."""
+
+
+class IncorrectPassword(LoginError):
+    """Raised when the password given at login is incorrect."""
 
 
 class CodinGamerNotFound(CodinGameAPIError):
