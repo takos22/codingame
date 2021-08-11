5ffe9f48bb44e4cd7c68bce157025aa6fdf65e8f
Takos22 <takos2210@gmail.com>
2021-08-11 21:07:28 +0200

    Add http error mocking

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import typing
 
 from pytest_mock import MockerFixture
 
+from codingame.http import HTTPError
+
 if typing.TYPE_CHECKING:
     from _pytest.config import Config
     from _pytest.config.argparsing import Parser
@@ -13,11 +15,17 @@ if typing.TYPE_CHECKING:
 
 def pytest_addoption(parser: "Parser"):
     parser.addoption(
+        "--nm",
         "--no-mocking",
         action="store_true",
-        default=False,
+        dest="no_mocking",
         help="Run tests with API calls",
     )
+
+
+@pytest.fixture(name="is_mocking")
+def is_mocking_fixture(pytestconfig: "Config"):
+    return not pytestconfig.getoption("no_mocking", default=False)
 
 
 class FakeMocker:
@@ -25,19 +33,15 @@ class FakeMocker:
         pass
 
     def __getattr__(self, _):
-        return FakeMocker()
+        return FakeMocker()  # for attribute of attribute
 
     def __call__(self, *_, **__):
         pass
 
 
 @pytest.fixture(name="mocker")
-def mocker_fixture(pytestconfig: "Config"):
-    mocker = (
-        FakeMocker
-        if pytestconfig.getoption("--no-mocking", default=True)
-        else MockerFixture
-    )
+def mocker_fixture(is_mocking: bool, pytestconfig: "Config"):
+    mocker = MockerFixture if is_mocking else FakeMocker
 
     # imitate https://github.com/pytest-dev/pytest-mock/blob/main/src/pytest_mock/plugin.py#L388 # noqa: E501
     result = mocker(pytestconfig)
@@ -72,3 +76,26 @@ def mock_http_fixture(mocker: MockerFixture):
         )
 
     return mock_http
+
+
+@pytest.fixture(name="mock_httperror")
+def mock_httperror_fixture(mocker: MockerFixture):
+    def mock_httperror(
+        http_client: "HTTPClient", method: str, api_data, *args, **kwargs
+    ):
+        error = HTTPError(422, "", api_data)
+        if http_client.is_async:
+
+            async def fake_api_call(*_):
+                raise error
+
+        else:
+
+            def fake_api_call(*_):
+                raise error
+
+        mocker.patch.object(
+            http_client, method, new=fake_api_call, *args, **kwargs
+        )
+
+    return mock_httperror
