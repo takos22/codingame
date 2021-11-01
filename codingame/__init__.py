074165f73a6e169c39cb0fc9d26175de422c4b2b
Takos22 <takos2210@gmail.com>
2021-11-01 23:44:42 +0100

    Bump version to 1.1.0

--- codingame/__init__.py
+++ codingame/__init__.py
@@ -11,13 +11,11 @@ VersionInfo = NamedTuple(
     "VersionInfo", major=int, minor=int, micro=int, releaselevel=str, serial=int
 )
 
-version_info = VersionInfo(
-    major=1, minor=1, micro=0, releaselevel="alpha", serial=0
-)
+version_info = VersionInfo(major=1, minor=1, micro=0, releaselevel="", serial=0)
 
 __title__ = "codingame"
 __author__ = "takos22"
-__version__ = "1.1.0a"
+__version__ = "1.1.0"
 
 from .clash_of_code import ClashOfCode, Player
 from .client import Client
