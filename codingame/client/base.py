bffb0c3f10d5f3417b55cf642fec0cc113b78e50
Takos22 <takos2210@gmail.com>
2021-11-04 18:15:50 +0100

    Update Client.login docstring

--- codingame/client/base.py
+++ codingame/client/base.py
@@ -155,15 +155,9 @@ class BaseClient(ABC):
 
         .. error::
             As of 2021-10-27, the only way to login is with cookie
-            authentication, so with the``remember_me_cookie`` parameter, because
-            of an endpoint change, see :ref:`login`. Using email/password
-            authentication will raise a :exc:`LoginError`.
-
-        .. error::
-            As of 2021-10-27, the only way to login is with cookie
-            authentication, so with the``remember_me_cookie`` parameter, because
-            of an endpoint change, see :ref:`login`. Using email/password
-            authentication will raise a :exc:`LoginError`.
+            authentication, so with the ``remember_me_cookie`` parameter,
+            because of an endpoint change, see :ref:`login`. Using
+            email/password authentication will raise a :exc:`LoginError`.
 
         Parameters
         -----------
